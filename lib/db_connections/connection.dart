3e48b9ab452416eaac3002fa14a5b6c38203ab4b
Inderpal Singh <inderpalsmutta@gmail.com>
2024-03-30 16:38:29 +0530

    database setup

---
+++ lib/db_connections/connection.dart
@@ -0,0 +1,63 @@
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DbAppConnection {
+  
+  /// Singleton (private constructor )
+  DbAppConnection._();
+
+  /// accessing the connection class object and create db class object
+  static final DbAppConnection db = DbAppConnection._();
+
+  /// all db logic
+
+  Database? myDb;
+
+  Future<Database> getDB() async {
+    if (myDb != null) {
+      return myDb!;
+    } else {
+     myDb = await initDb();
+     return myDb!;
+    }
+  }
+
+  
+  Future<Database> initDb() async {
+    var rootPath = await getApplicationDocumentsDirectory();
+    var actualPath = join(rootPath.path, 'todo_db.db');
+
+    /// db create
+    return await openDatabase( actualPath, version: 1, onCreate: (db, version) async {
+
+      /// create table
+      await db.execute('CREATE TABLE todo (id INTEGER PRIMARY KEY AUTOINCREMENT, todo_title TEXT, todo_desc TEXT )');
+        
+      },
+    );
+  }
+  
+
+  /// insert data
+  Future addTodo(String title, String desc) async{
+    var db = await getDB();
+    
+    db.insert('todo', {
+      'todo_title': title,
+      'todo_desc': desc
+    });
+    
+  }
+
+  /// fetch data
+  Future<List<Map<String, dynamic>>> fetchAllTodo()async{
+    
+    var db = await getDB();
+    var data = await db.query('todo');
+    return data;
+    
+    
+
+  }
+}
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:todo_sqlite/db_connections/connection.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  DbAppConnection? db;
+
+  
+  @override
+  void initState() {
+    var db = DbAppConnection.db; // accessing the database class object
+    super.initState();
+  }
+  
+  
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    
+    
+    
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo Sqlite'),
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return ListView();
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          db!.addTodo();
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
